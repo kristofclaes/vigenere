f2c4343d8c03cfa2afe50a0ca8bc40ef246da9ef
Kristof Claes <kristof.claes@unleashed.be>
2017-04-22 14:15:29 +0200

    Flake8 fixes.

--- src/vigenere/utils.py
+++ src/vigenere/utils.py
@@ -6,8 +6,8 @@ def pair_up_characters(text, key):
     """
     Takes a text for enciphering or deciphering and a key and pairs up each character from the text with the correct
     character from the key.
-    :param str text: 
-    :param str key: 
+    :param str text: The text that needs to be enciphered or deciphered.
+    :param str key: The key to use for enciphering or deciphering.
     :return: A list of tuples where the first element of the tuple is character from the text and the second element of
              the tuple is the corresponding character of the key.
     :rtype: list
